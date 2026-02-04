5a6fac18134127a0d5172bb3aa5d6a4cbad18c73
sbx_user1051 <sbx_user1051@169.254.18.209>
2026-02-04 16:13:52 +0000

    Wed Feb 04 16:13:52 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -98,8 +98,8 @@ def test_bank_statement_skip_2() -> None:
         call_enqueue(provider="companies_house", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(1),
         call_enqueue(provider="bank_statements", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(2),
         call_enqueue(provider="id_verification", user_id=6, timestamp=iso_ts(delta_minutes=6)).expect(3),
-        call_dequeue().expect("companies_house", 1),
         call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("companies_house", 1),
         call_dequeue().expect("id_verification", 6),
     ])
 
@@ -117,7 +117,7 @@ def test_bank_statement_skip_4() -> None:
     ])
 
 
-def test_bank_statement_skip_4() -> None:
+def test_bank_statement_skip_5() -> None:
     run_queue([
         call_enqueue(provider="bank_statements", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(1),
         call_enqueue(provider="companies_house", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(2),
@@ -126,6 +126,7 @@ def test_bank_statement_skip_4() -> None:
         call_dequeue().expect("companies_house", 1),
         call_dequeue().expect("id_verification", 6),
     ])
+
 
 
 
